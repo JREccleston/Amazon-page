0515a8f0661feac8b64ece85e4ecdc805958b575
John.Ecc <jreccleston@cox.net>
2023-11-03 10:32:00 -0500

    Updated Payment Summary

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,7 +3,9 @@ import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 import { hello } from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
-import { deliveryOptions } from "../data/deliveryOptions.js";
+import { deliveryOptions, getDeliveryOption } from "../data/deliveryOptions.js";
+import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+import { getproduct } from "../data/products.js";
 
 const today = dayjs();
 const deliveryDate = today.add(7, 'days');
@@ -15,21 +17,10 @@ function renderOrderSummary() {
     cart.forEach((cartItem) => {
 
         const productId = cartItem.productId;
-        let matchingProduct;
-
-        products.forEach((product) => {
-            if (product.id === productId) {
-                matchingProduct = product;
-            }
-        })
+        const matchingProduct = getproduct(productId);
 
         const deliveryOptionId = cartItem.deliveryOptionId;
-        let deliveryOption;
-        deliveryOptions.forEach((option) => {
-            if (option.id === deliveryOptionId) {
-                deliveryOption = option;
-            }
-        })
+        const deliveryOption = getDeliveryOption(deliveryOptionId);
         const today = dayjs();
         const deliveryDate = today.add(deliveryOption.deliveryDays, 'days');
         const dateString = deliveryDate.format('dddd, MMMM, D');
@@ -125,3 +116,4 @@ function renderOrderSummary() {
 
 
 renderOrderSummary();
+renderPaymentSummary();
